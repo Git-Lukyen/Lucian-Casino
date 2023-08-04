336465ad831e08d45485544fdf5e9ee488780964
Lucian <dracea.lucian.mihai@gmail.com>
2023-08-04 19:25:38 +0300

    worked on the login page and added a cool parallax effect :D

---
+++ LucianCasino/wwwroot/js/login-page.js
@@ -0,0 +1,10 @@
+﻿$( document ).ready(function() {
+
+    $(document).mousemove(function(e){
+        let background = $(".background img");
+        let offX = e.pageX * -0.1;
+        let offY = e.pageY * -0.1;
+        background.css("transform", `translateX(${offX}px) translateY(${offY}px`);
+    });
+
+});
