44f3c1ec06f2c62ab096d28e01c23034a54af720
Lucian <dracea.lucian.mihai@gmail.com>
2023-08-05 22:26:23 +0300

    fixed a bug where the reload would offset the div's position

--- LucianCasino/wwwroot/js/login-page.js
+++ LucianCasino/wwwroot/js/login-page.js
@@ -1,10 +1,10 @@
-﻿$( document ).ready(function() {
+﻿$(document).ready(function () {
 
-    $(document).mousemove(function(e){
+    $(document).mousemove(function (e) {
         let background = $(".background img");
-        let offX = e.pageX * -0.1;
-        let offY = e.pageY * -0.1;
+        let offX = e.clientX * -0.1;
+        let offY = e.clientY * -0.1;
         background.css("transform", `translateX(${offX}px) translateY(${offY}px`);
     });
-
+    
 });
